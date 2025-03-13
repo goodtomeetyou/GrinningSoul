0b6758af15c02cafb7ab6c4512df8b44391c0e8b
Jesus Martinez <jesse_m@naver.com>
2025-03-12 18:18:50 -0600

    Update convertOne.sh

--- convertOne.sh
+++ convertOne.sh
@@ -2,6 +2,4 @@
 
 echo "Converting $1"
 python wrap.py "$1" "convertedApps/$(basename "$1")";
-#xcrun simctl install 4AE7240F-67C1-4197-9639-235C2E6417A1 "convertedApps/$(basename "$1")";
-xcrun simctl install D1A6DE6D-4268-4FC5-8BD2-6083F1E3BD33 "convertedApps/$(basename "$1")";
-#xcrun simctl install 5FF1783B-C870-4688-8D3D-8B8CC6A818C0 "convertedApps/$(basename "$1")";
+#xcrun simctl install "your simulator device udid" "convertedApps/$(basename "$1")";
